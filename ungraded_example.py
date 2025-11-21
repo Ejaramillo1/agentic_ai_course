5fd079c4c0c8a286213dd7389c2e302551852de8
Ed <ejaramillobarrera@gmail.com>
2025-11-21 07:17:33 -0800

    this is my attempt to follow the course

---
+++ ungraded_example.py
@@ -0,0 +1,10 @@
+# Import core python libraries
+
+import json
+import utils
+import pandas as pd
+from dotenv import load_dotenv
+_ = load_dotenv()  # Load environment variables from .env file
+
+
+
